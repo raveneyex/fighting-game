d7884d4768d7db6ff174f74c5ee5be861bbb88d2
Andres Ossa <aossa@vmware.com>
2023-03-04 18:58:21 -0500

    With timers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Fighter, Sprite } from "./models/";
 import { ControlKeys } from "./models/types";
-import { detectCollition } from "./utils/utils";
+import { detectCollition, determineWinner } from "./utils/utils";
 
 import "./style.css";
 import * as backgroundImageSrc from "./assets/background.png";
@@ -19,7 +19,7 @@ const background = new Sprite({
   canvas,
 });
 
-const player = new Fighter({
+const player1 = new Fighter({
   position: {
     x: 0,
     y: 0,
@@ -36,7 +36,7 @@ const player = new Fighter({
   },
 });
 
-const enemy = new Fighter({
+const player2 = new Fighter({
   position: {
     x: 800,
     y: 50,
@@ -74,47 +74,63 @@ const keys = {
   },
 };
 
+let timer = 10;
+let timerId: number;
+
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = window.setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer.toString();
+  }
+
+  if (timer === 0) {
+    determineWinner(player1, player2, timerId);
+  }
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
-  player.update();
-  enemy.update();
+  player1.update();
+  player2.update();
 
-  player.velocity.x = 0;
-  if (keys.a.pressed && player.lastKey === ControlKeys.a) {
-    player.velocity.x = -5;
-  } else if (keys.d.pressed && player.lastKey === ControlKeys.d) {
-    player.velocity.x = 5;
+  player1.velocity.x = 0;
+  if (keys.a.pressed && player1.lastKey === ControlKeys.a) {
+    player1.velocity.x = -5;
+  } else if (keys.d.pressed && player1.lastKey === ControlKeys.d) {
+    player1.velocity.x = 5;
   }
 
-  enemy.velocity.x = 0;
-  if (keys.ArrowLeft.pressed && enemy.lastKey === ControlKeys.ArrowLeft) {
-    enemy.velocity.x = -5;
+  player2.velocity.x = 0;
+  if (keys.ArrowLeft.pressed && player2.lastKey === ControlKeys.ArrowLeft) {
+    player2.velocity.x = -5;
   } else if (
     keys.ArrowRight.pressed &&
-    enemy.lastKey === ControlKeys.ArrowRight
+    player2.lastKey === ControlKeys.ArrowRight
   ) {
-    enemy.velocity.x = 5;
+    player2.velocity.x = 5;
   }
 
-  if (detectCollition(player, enemy) && player.isAttacking) {
-    player.isAttacking = false;
-    enemy.health -= 20;
+  if (detectCollition(player1, player2) && player1.isAttacking) {
+    player1.isAttacking = false;
+    player2.health -= 20;
     const player2HealthHTMLElement: HTMLDivElement =
       document.querySelector("#player2HealthBar");
-    player2HealthHTMLElement.style.width = `${enemy.health}%`;
+    player2HealthHTMLElement.style.width = `${player2.health}%`;
   }
 
-  if (detectCollition(enemy, player) && enemy.isAttacking) {
-    enemy.isAttacking = false;
-    player.health -= 20;
+  if (detectCollition(player2, player1) && player2.isAttacking) {
+    player2.isAttacking = false;
+    player1.health -= 20;
     const player1HealthHTMLElement: HTMLDivElement =
       document.querySelector("#player1HealthBar");
-    player1HealthHTMLElement.style.width = `${player.health}%`;
+    player1HealthHTMLElement.style.width = `${player1.health}%`;
   }
 }
+decreaseTimer();
 animate();
 
 window.addEventListener("keydown", (event: KeyboardEvent) => {
@@ -124,25 +140,25 @@ window.addEventListener("keydown", (event: KeyboardEvent) => {
     case ControlKeys.d:
     case ControlKeys.a:
       keys[key].pressed = true;
-      player.lastKey = key;
+      player1.lastKey = key;
       break;
     case ControlKeys.w:
-      player.velocity.y = -20;
+      player1.velocity.y = -20;
       break;
     case ControlKeys.space:
-      player.attack();
+      player1.attack();
       break;
 
     case ControlKeys.ArrowRight:
     case ControlKeys.ArrowLeft:
       keys[key].pressed = true;
-      enemy.lastKey = key;
+      player2.lastKey = key;
       break;
     case ControlKeys.ArrowUp:
-      enemy.velocity.y = -20;
+      player2.velocity.y = -20;
       break;
     case ControlKeys.ArrowDown:
-      enemy.attack();
+      player2.attack();
       break;
   }
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,29 @@
 import { Fighter } from "../models";
 
-export function detectCollition(player: Fighter, enemy: Fighter) {
+export function detectCollition(player1: Fighter, player2: Fighter) {
   return (
-    player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-    player.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height
+    player1.attackBox.position.x + player1.attackBox.width >=
+      player2.position.x &&
+    player1.attackBox.position.x <= player2.position.x + player2.width &&
+    player1.position.y + player1.attackBox.height >= player2.position.y &&
+    player1.attackBox.position.y <= player2.position.y + player2.height
   );
+}
+
+export function determineWinner(
+  player1: Fighter,
+  player2: Fighter,
+  timerId: number
+) {
+  clearTimeout(timerId);
+  const displayText: HTMLDivElement = document.querySelector("#message");
+
+  displayText.style.display = "flex";
+  if (player1.health === player2.health) {
+    displayText.innerHTML = "It's a tie!";
+  } else if (player1.health > player2.health) {
+    displayText.innerHTML = "Player 1 Wins";
+  } else if (player1.health < player2.health) {
+    displayText.innerHTML = "Player 2 Wins";
+  }
 }
