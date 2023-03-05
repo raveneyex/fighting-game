e4721728c108d6259dcc98c5f814308f389e06cf
Andres Ossa <aossa@vmware.com>
2023-03-04 23:30:25 -0500

    Export player assets

---
+++ src/assets/player2/index.ts
@@ -0,0 +1,30 @@
+import { SpriteListing } from "../../models";
+
+import * as idle from "./Idle.png";
+import * as run from "./Run.png";
+import * as jump from "./Jump.png";
+import * as fall from "./Fall.png";
+import * as attack from "./Attack2.png";
+
+export const player2Assets: SpriteListing = {
+  idle: {
+    imageSrc: idle as unknown as string,
+    frames: 8,
+  },
+  run: {
+    imageSrc: run as unknown as string,
+    frames: 8,
+  },
+  jump: {
+    imageSrc: jump as unknown as string,
+    frames: 2,
+  },
+  fall: {
+    imageSrc: fall as unknown as string,
+    frames: 2,
+  },
+  attack: {
+    imageSrc: attack as unknown as string,
+    frames: 6,
+  },
+};
